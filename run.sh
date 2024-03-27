526cf044100a4ba7419b23ebeef41b42aeb65935
Ashowman98 <152960940+Ashowman98@users.noreply.github.com>
2024-03-27 13:52:08 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 #do
 #    for s in a b c d e
 #    do
-#          python3 main.py --gpu 0 --ds ./exps/$dataset/spl_$s.json --config ./configs/pcssr/$dataset.json --save $dataset/$s'_m5' --method csgrl --test_interval 1
+#          python3 main.py --gpu 0 --ds ./exps/$dataset/spl_$s.json --config ./configs/csgrl/$dataset.json --save $dataset/$s'_m5' --method csgrl --test_interval 1
 #    done
 #done
 
 for dataset in ood
 do
-  python3 main_ood.py --gpu 0 --ds ./exps/cifar10/$dataset"_spl".json --config ./configs/pcssr/cifar10.json --save '_m5' --method csgrl --test_interval 1
+  python3 main_ood.py --gpu 0 --ds ./exps/cifar10/$dataset"_spl".json --config ./configs/csgrl/cifar10.json --save '_m5' --method csgrl --test_interval 1
 done
